9cf2985674d8968a02bff903a63cc72c269303eb
emaphp <emmanuel.antico@gmail.com>
2014-03-13 15:14:51 -0300

    Added: SQLite dynamic attribute tests

--- tests/eMapper/SQLite/SQLiteTest.php
+++ tests/eMapper/SQLite/SQLiteTest.php
@@ -3,6 +3,7 @@ namespace eMapper\SQLite;
 
 use eMapper\Engine\SQLite\SQLiteMapper;
 use Acme\Type\RGBColorTypeHandler;
+use eMapper\SQL\Statement;
 
 abstract class SQLiteTest extends \PHPUnit_Framework_TestCase {
 	public static $env_config = ['environment.id' => 'default', 'environment.class' => 'eMapper\Dynamic\Environment\DynamicSQLEnvironment'];
@@ -18,6 +19,8 @@ abstract class SQLiteTest extends \PHPUnit_Framework_TestCase {
 		
 		self::$mapper = new SQLiteMapper(self::$filename);
 		self::$mapper->addType('Acme\RGBColor', new RGBColorTypeHandler(), 'color');
+		self::$mapper->addStatement(new Statement('getProduct', "SELECT * FROM products WHERE product_id = #{productId}"));
+		self::$mapper->addStatement(new Statement('getUser', "SELECT * FROM users WHERE user_id = %{int}", Statement::type('array')));
 		
 		self::$blob = file_get_contents(__DIR__ . '/../avatar.gif');
 	}
