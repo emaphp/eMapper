f5fcd997540dc8a42e9ee6d700a290631961b334
emaphp <emmanuel.antico@gmail.com>
2014-08-28 19:22:50 -0300

    Added: Suport for @Index and @OrderBy annotations on associations.

--- lib/eMapper/Reflection/Profile/Association/Association.php
+++ lib/eMapper/Reflection/Profile/Association/Association.php
@@ -6,6 +6,8 @@ use eMapper\Annotations\Annotation;
 use eMapper\Manager;
 use eMapper\Annotations\AnnotationsBag;
 use eMapper\AssociationManager;
+use eMapper\Annotations\Filter;
+use eMapper\Query\Attr;
 
 /**
  * The Association class encapsulates common logic between the various types of entity associations.
@@ -38,6 +40,18 @@ abstract class Association extends PropertyProfile {
 	 * @var boolean
 	 */
 	protected $lazy;
+	
+	/**
+	 * Index attribute
+	 * @var string
+	 */
+	protected $index;
+	
+	/**
+	 * Order criteria
+	 * @var array
+	 */
+	protected $order = [];
 		
 	public function __construct($entity, $name, AnnotationsBag $annotations, \ReflectionProperty $reflectionProperty) {
 		try {
@@ -61,6 +75,15 @@ abstract class Association extends PropertyProfile {
 		$this->attribute = $annotations->has('Attr') ? $annotations->get('Attr') : null;
 		$this->joinWith = $annotations->has('JoinWith') ? $annotations->get('JoinWith') : null;
 		$this->lazy = $annotations->has('Lazy');
+		
+		//parse options
+		$this->index = $annotations->has('Index') ? $annotations->get('Index')->getValue() : null;
+		
+		$order = $annotations->find('OrderBy', Filter::HAS_ARGUMENT);
+		
+		foreach ($order as $option) {
+			$this->order[$option->getArgument()] = $option->getValue();
+		}
 		
 		$this->reflectionProperty = $reflectionProperty;
 		$this->reflectionProperty->setAccessible(true);
@@ -102,6 +125,29 @@ abstract class Association extends PropertyProfile {
 		
 		//build association manager
 		$manager = new AssociationManager($mapper, $this, $condition);
+		
+		//apply indexation
+		if (!empty($this->index) && is_string($this->index)) {
+			$manager = $manager->index(Attr::__callstatic($this->index));
+		}
+		
+		//apply order
+		if (!empty($this->order)) {
+			$order = [];
+			
+			foreach ($this->order as $key => $value) {
+				if (is_boolean($value) || (strtolower($value) != 'asc' && strtolower($value) != 'desc')) {
+					$type = null;
+				}
+				else {
+					$type = $value;
+				}
+				
+				$order[] = Attr::__callstatic($key, [$type]);
+			}
+			
+			$manager = $manager->merge(['query.order' => $order]);
+		}
 		
 		if ($this->lazy) {
 			return $manager;
